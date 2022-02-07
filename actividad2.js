797c2d4c6e4f69544e4e39dbf3e49e59d2c2b8d2
Marcos Rodas <20062839@galileo.edu>
2022-02-06 20:47:33 -0600

    first commit

---
+++ actividad2.js
@@ -0,0 +1,33 @@
+
+// Crear una variable llamada mes y que tenga asignado por defecto el valor 10.
+// Crear un programa que verifique si el valor de mes está entre 12 y 2 (12, 1 o 2), entonces que imprima en pantalla “es invierno”, si el valor de mes está entre 3 y 5, que imprima en pantalla “es primavera”, si el valor de mes se encuentra entre 6 y 8 que imprima en pantalla “es verano” y si se encuentra entre 9 y 11 que imprima en pantalla “es otoño”.
+// Al finalizar debe imprimir en pantalla un mensaje que indique su número de carné y su nombre completo.
+
+
+
+var mes = 10
+if (mes == 1) {
+    console.log("Es Invieno")
+}else if (mes == 2){
+    console.log("Es Invierno")
+}else if (mes == 3){
+    console.log("Es Primavera")
+}else if (mes == 4){
+    console.log("Es Primavera")
+}else if (mes == 5){
+    console.log("Es Primavera")
+}else if (mes == 6){
+    console.log("Es Verano")
+}else if (mes == 7){
+    console.log("Es verano")
+}else if ( mes == 8){
+    console.log("Es Verano")
+}else if ( mes == 9){
+    console.log("Es Otoño")
+}else if (mes == 10){
+    console.log("Es Otoño")
+}else if (mes == 11){
+    console.log("Es Otoño")
+}else if (mes == 12){
+    console.log("Es Invierno")
+} alert ("Marcos Rodas 20062839")
